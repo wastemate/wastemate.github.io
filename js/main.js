68a0e0fc86a76a3e652d12165494bc7759da49fd
Anne Tsugunyan <anne.tsugunyan@gmail.com>
2016-03-30 01:27:59 +0400

    Basic pricing page

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$('.slider-input').jRange({
+    from: 10,
+    to: 100,
+    step: 1,
+    scale: [10,20,30,40,50,100],
+    format: '%s',
+    width: 300,
+    showLabels: false,
+    snap: true,
+});
